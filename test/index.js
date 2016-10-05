50f7e06ad5652374449d16cee3f0e6270a3f1606
raymondsquared <raymond_squared@live.com>
2016-10-05 23:42:00 +1100

    Creating a Simple Job

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var expect    = require("chai").expect;
+
+describe("Given index.js", function() {
+  describe("describe", function() {
+    it("it", function() {
+      expect(1).to.equal(1);
+    });
+  });
+});
